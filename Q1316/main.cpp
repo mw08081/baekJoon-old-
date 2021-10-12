528a2713182c53229a8be2eb6550d480f54f935f
mw304 <kksmiss34@gmail.com>
2021-10-12 14:16:33 +0900

    add Q1316, Q1712

--- Q1316/main.cpp
+++ Q1316/main.cpp
@@ -1,11 +1,48 @@
-//(start date)Q1316
+//21.10.12 Q1316
 
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-	cout << "Q 1316" << endl;
+	int length;
+	cin >> length;
 
+	string strs[100];
+	for (int i = 0; i < length; i++)
+		cin >> strs[i];
+
+	int stack = 1;
+	bool isBreak = false;
+	int cnt = 0;
+
+	for (int x = 0; x < length; x++)
+	{
+		stack = 1;
+		isBreak = false;
+		for (int i = 1; i < strs[x].length(); i++)
+		{
+			if (strs[x][i] == strs[x][i - 1])
+			{
+				stack++;
+				continue;
+			}
+			else
+				stack = 1;
+
+			for (int j = 0; j < i - stack; j++)
+				if (strs[x][i] == strs[x][j])
+					isBreak = true;
+
+			if (isBreak)
+				break;
+		}
+		if (!isBreak)
+			cnt++;
+	}
+	
+	cout << cnt << endl;
+	
 	return 0;
 }
---
+++ Q1712/main.cpp
@@ -0,0 +1,12 @@
+//21.10.12 Q1712
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+
+	return 0;
+}
