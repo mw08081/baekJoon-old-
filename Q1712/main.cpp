4d424b0d99a0703399c1c07ad13e78a843cc6f17
mw304 <kksmiss34@gmail.com>
2021-10-12 15:31:22 +0900

    add 1712 helped

--- Q1712/main.cpp
+++ Q1712/main.cpp
@@ -6,7 +6,16 @@ using namespace std;
 
 int main()
 {
-	
+	int staticPrice;
+	int valuablePrice;
+	int salePrice;
+
+	cin >> staticPrice >> valuablePrice >> salePrice;
+
+	if (valuablePrice >= salePrice)
+		cout << -1 << endl;
+	else
+		cout << staticPrice / (salePrice - valuablePrice) + 1 << endl;
 
 	return 0;
 }
