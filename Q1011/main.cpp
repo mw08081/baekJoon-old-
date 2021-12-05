d1e84ad0270f8b013ae9b9c66f021da668243f28
mw08081 <president304@naver.com>
2021-12-05 20:15:11 +0900

    added 1011

---
+++ Q1011/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int T;
+    int st, end;
+    int step, n = 1, remain, cnt;
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        cin >> st >> end;
+        step = end - st;
+        cnt = 0;
+
+        // while(n * n <= step)
+        //    n++;
+        // n--;
+        n = sqrt(step);
+
+        remain = step - n * n;
+        cnt += (n - 1) * 2 + 1;
+
+         while(remain > 0)
+        {
+            remain -= n;
+            cnt++;
+
+            if(remain < n)
+                n--;
+        }
+        cout << cnt << '\n';
+    }
+    return 0;
+}
