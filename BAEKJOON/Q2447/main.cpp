3161c4ff4dab03aefac1b17bd1d3b8166e068c14
mw08081 <president304@naver.com>
2021-12-14 16:18:58 +0900

    added 2447

---
+++ BAEKJOON/Q2447/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void RecursiveDraw(int n, int k, int i, int j)
+{
+    if(i == 0)        
+        return;
+
+    if(j == 0)
+    {
+        cout << "\n";
+        RecursiveDraw(n, n / 3, i - 1, n);
+        return;
+    }
+
+    if(k > 0)
+    {
+        if(((n * 2 / 3 >= i) && (n * 1 / 3 + 1 <= i) && (n * 2 / 3  >= j) && (n * 1 / 3 + 1 <= j)) || (i % k >= (k / 3 + 1) && i % k <= (k / 3 * 2)) && (j % k >= (k / 3 + 1) && j % k <= (k / 3 * 2)))
+        {
+            cout << " ";
+            RecursiveDraw(n, n / 3, i, j - 1); 
+        }
+        else
+            RecursiveDraw(n, k / 3, i, j);    
+    }
+    else
+    {
+        cout << "*";
+        RecursiveDraw(n, n / 3, i, j - 1);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    RecursiveDraw(n, n / 3, n, n);        
+
+    return 0;
+}
