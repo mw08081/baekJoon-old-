cc9faf705ea9ca6495dff715d4912b4c6a99c17b
mw08081 <president304@naver.com>
2021-12-28 21:20:20 +0900

    added 1436

--- BAEKJOON/Q1436/main.cpp
+++ BAEKJOON/Q1436/main.cpp
@@ -3,6 +3,29 @@ using namespace std;
 
 int main()
 {
+    int n, i = 666, cnt = 0;
+    cin >> n;
 
+    while (n != cnt)
+    {   
+        int tmp = i, tmpCnt = 0;
+        while (tmp >= 1)
+        {
+            if(tmp % 10 == 6)
+                tmpCnt++;
+            else
+                tmpCnt = 0;
+
+            tmp /= 10;
+            if(tmpCnt == 3)
+                break;
+        }
+
+        if(tmpCnt == 3)
+            cnt++;
+        i++;
+    }
+    cout << i - 1 << '\n';
+   
     return 0;
 }
