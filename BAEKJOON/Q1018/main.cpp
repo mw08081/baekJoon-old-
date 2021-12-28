2db2efe4f9316af256958166172092e40083cb7c
mw08081 <president304@naver.com>
2021-12-28 20:40:59 +0900

    added 1018

--- BAEKJOON/Q1018/main.cpp
+++ BAEKJOON/Q1018/main.cpp
@@ -3,92 +3,55 @@ using namespace std;
 
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    char ** board = new char*[n];
-    char ** editBoard = new char*[n];
-
-    for (int i = 0; i < n; i++)
+    char arrCaseA[8][8] = 
     {
-        board[i] = new char[m];
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+    };
+    char arrCaseB[8][8] = 
+    {
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+    };
+    char input[50][50] = {0};
+    int n, m, min = 33, cnt = 0;
+    cin >> n >> m;
+    
+    for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
-            cin >> board[i][j];
-        editBoard[i] = new char[m];
-    }
+            cin >> input[i][j];
 
-    int cnt = 0, min = 65;
     for (int i = 0; i < n - 7; i++)
     {
         for (int j = 0; j < m - 7; j++)
-        {   
-            for (int p = 0; p < n; p++)
-                for (int q = 0; q < m; q++)
-                    editBoard[p][q] = board[p][q];
-            
+        {
             cnt = 0;
             for (int p = i; p < 8 + i; p++)
-            {
                 for (int q = j; q < 8 + j; q++)
-                {
-                    if(p > 0 && q == 0 && editBoard[p - 1][q] == editBoard[p][q])
-                    {
+                    if (input[p][q] != arrCaseA[p - i][q - j])
                         cnt++;
-                        if(editBoard[p][q] == 'B')
-                            editBoard[p][q] = 'W';
-                        else
-                            editBoard[p][q] = 'B';
-                    }
-
-                    if(editBoard[p][q + 1] == editBoard[p][q])
-                    {
-                        cnt++;
-                        if(editBoard[p][q + 1] == 'B')
-                            editBoard[p][q + 1] = 'W';
-                        else
-                            editBoard[p][q + 1] = 'B';
-                    }
-                        
-                }   
-            }   
             if(cnt < min)
                 min = cnt;
-        }
-    }
-    for (int i = 0; i < n - 7; i++)
-    {
-        for (int j = m - 1; j > 6; j--)
-        {   
-            for (int p = 0; p < n; p++)
-                for (int q = 0; q < m; q++)
-                    editBoard[p][q] = board[p][q];
-            
+
             cnt = 0;
             for (int p = i; p < 8 + i; p++)
-            {
-                for (int q = j; q > j - 7; q--)
-                {
-                    if(p > 0 && q == m - 1 && editBoard[p - 1][q] == editBoard[p][q])
-                    {
+                for (int q = j; q < 8 + j; q++)
+                    if (input[p][q] != arrCaseB[p - i][q - j])
                         cnt++;
-                        if(editBoard[p][q] == 'B')
-                            editBoard[p][q] = 'W';
-                        else
-                            editBoard[p][q] = 'B';
-                    }
-
-                    if(editBoard[p][q - 1] == editBoard[p][q])
-                    {
-                        cnt++;
-                        if(editBoard[p][q - 1] == 'B')
-                            editBoard[p][q - 1] = 'W';
-                        else
-                            editBoard[p][q - 1] = 'B';
-                    }
-                        
-                }   
-            }   
             if(cnt < min)
-                min = cnt;               
+                min = cnt;
         }
     }
     cout << min << '\n';
