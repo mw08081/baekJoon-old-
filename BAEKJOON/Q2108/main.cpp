7e525ce453f31ab307f604c5bcf5b909c00acf72
mw08081 <president304@naver.com>
2022-01-18 16:39:43 +0900

    modified 2108

--- BAEKJOON/Q2108/main.cpp
+++ BAEKJOON/Q2108/main.cpp
@@ -9,118 +9,130 @@ void Swap(int * n1, int * n2)
     *n1 = *n2;
     *n2 = tmp;
 }
-
-void QuickSort(int * arr, int l, int r)
+void InsertData(vector<int> * arr, int n)
 {
-    if (l < r)
+    for (int i = 0; i < n; i++)
     {
-        int p = 0;
+        int tmp;
+        cin >> tmp;
 
-        for (int i = 0; i < r; i++)
-        {
-            if(arr[i] < arr[r])
-            {
-                Swap(&arr[p], &arr[i]);
-                p++;
-            }
-        }
-        Swap(&arr[p], &arr[r]);
-
-        QuickSort(arr, l, p - 1);
-        QuickSort(arr, p + 1, r);
+        arr->push_back(tmp);
     }
 }
+vector<int> HeapSort(vector<int> * arr)
+{
+    vector<int> res {0};
+    int len = arr->size() - 1;
 
-int GetAvg(int * arr, int n)
+    //MAXHEAPIFY
+    for (int i = len / 2; i > 0; i--)
+    {
+        int p = i, c = p * 2;
+        while(len >= c)
+        {
+            if(len >= c + 1 && arr->at(c + 1) > arr->at(c)) c++;
+            if(arr->at(p) >= arr->at(c)) break;
+
+            Swap(&arr->at(p), &arr->at(c));
+            p = c;
+            c = p * 2;
+        }
+    }
+
+    //EXTRACT ELEMENT
+    while(arr->size() > 1)
+    {
+        Swap(&arr->at(1), &arr->at(arr->size() - 1));
+        res.push_back(arr->back());
+        arr->pop_back();
+
+        int p = 1, c = p * 2;
+        while(arr->size() - 1 >= c)
+        {
+            if(arr->size() - 1 >= c + 1 && arr->at(c + 1) >= arr->at(c)) c++;
+            if(arr->at(p) >= arr->at(c)) break;
+
+            Swap(&arr->at(p), &arr->at(c));
+
+            p = c;
+            c = p * 2;
+        }
+    }
+    for (int i = 1; i <= len / 2; i++)
+        Swap(&res.at(i), &res.at(len - i + 1));
+
+    return res;       
+}
+
+int GetAvg(vector<int> * arr)
 {
     double tot = 0;
-    for (int i = 0; i < n; i++)
-        tot += arr[i];
+    for (int i = 1; i < arr->size(); i++)
+        tot += arr->at(i);
     
-    return floor(tot/n + 0.5f);
+    return floor(tot/(arr->size() - 1) + 0.5f);
 }
-
-int GetMid(int * arr, int n)
+int GetMid(vector<int> * arr)
 {
-    return arr[n / 2];
+    return arr->at((arr->size() - 1) / 2 + 1);
 }
-
-int GetMode(int * arr, int n)
+int GetMode(vector<int> * arr)
 {
-    if(n == 1)
-        return arr[0];
-
+    if(arr->size() == 2)
+        return arr->at(1);
     vector<int> vec;
 
-    int tmp = 1, i, maxMode = -4000001;
-    for (i = 1; i < n; i++)
+    int tmp = 1, i, maxCnt = 0;
+    for (i = 2; i < arr->size(); i++)
     {
-        if(arr[i - 1] == arr[i])
+        if(arr->at(i - 1) == arr->at(i))
             tmp++;
         else//if(arr[i - 1] != arr[i])
         {
-            if(maxMode < tmp)
+            if(maxCnt < tmp)
             {
                 vec.clear();
-                maxMode = tmp;
-                vec.push_back(arr[i - 1]);
+                maxCnt = tmp;
+                vec.push_back(arr->at(i - 1));
             }
-            else if(maxMode == tmp)
-            {
-                vec.push_back(arr[i - 1]);
-            }
+            else if(maxCnt == tmp)
+                vec.push_back(arr->at(i - 1));
             tmp = 1;
         }
     }
-    if(maxMode < tmp)
+    if(maxCnt < tmp)
     {
         vec.clear();
-        maxMode = tmp;
-        vec.push_back(arr[i - 1]);
+        maxCnt = tmp;
+        vec.push_back(arr->at(i - 1));
     }
-    else if(maxMode == tmp)
-    {
-        vec.push_back(arr[i - 1]);
-    }
+    else if(maxCnt == tmp)
+        vec.push_back(arr->at(i - 1));
     
-    // cout << "maxMode " << maxMode << endl << "list ";
-    // for (int i = 0; i < vec.size(); i++)
-    //     cout << vec.at(i) << " ";
-    // cout << endl;
-    
-    if(vec.size() > 2)
+    if(vec.size() > 1)
         return vec.at(1);
-    else if(vec.size() == 2)
-        return vec.at(0);
     else
         return vec.at(0);
 
 }
-
-int GetScope(int * arr, int n)
+int GetScope(vector<int> * arr)
 {
-    return arr[n - 1] - arr[0];
+    return arr->at(arr->size() - 1) - arr->at(1);
 }
 
 int main()
 {
-    int arr[500000];
-    
-
+    vector<int> arr {0};
     int n;
     cin >> n;
-    for (int  i = 0; i < n; i++)
-        cin >> arr[i];
-    
-    QuickSort(arr, 0, n - 1);
 
-    //cout << GetAvg(arr, n) << '\n';
-    //cout << GetMid(arr, n) << '\n';
-    cout << GetMode(arr, n) << '\n';
-    //cout << GetScope(arr, n) << '\n';
-    
-    
-    
+    InsertData(&arr, n);
+    arr = HeapSort(&arr);
 
+    cout << GetAvg(&arr) << '\n';
+    cout << GetMid(&arr) << '\n';
+    cout << GetMode(&arr) << '\n';
+    cout << GetScope(&arr) << '\n';
+    
     return 0;
 }
