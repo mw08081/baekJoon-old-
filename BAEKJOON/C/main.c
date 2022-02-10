9fe083de8186bb2fa53a1764b6e94e34fbec9e81
mw08081 <president304@naver.com>
2022-02-10 13:54:52 +0900

    added 10814

---
+++ BAEKJOON/C/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 3;
+    int num2 = 4;
+    int result = num1 + num2;
+
+    printf("덧셈 결과  %d\n", result);
+    printf("%d + %d = %d\n", num1, num2, num1 + num2);
+    printf("%d 와 %d 의 합은 %d입니다. \n", num1, num2, result);
+
+    return 0;
+}
--- BAEKJOON/Q10814/main.cpp
+++ BAEKJOON/Q10814/main.cpp
@@ -5,23 +5,60 @@
 #define MAX_LEN 100000
 using namespace std;
 
-vector<pair<int, string>> sorted(MAX_LEN);
+vector<pair<int, string> > sorted(MAX_LEN);
+
+void Merge(vector<pair<int, string> > * arr, int l, int m, int r)
+{
+    int i = l;
+    int j = m + 1;
+    int k = l;
+
+    while(i <= m && j <= r)
+    {
+        if(arr->at(i).first <= arr->at(j).first)
+            sorted.at(k++) = arr->at(i++);
+        else
+            sorted.at(k++) = arr->at(j++);
+    }
+
+    if(i > m)
+        for (int t = j; t <= r; t++)
+            sorted.at(k++) = arr->at(t);
+    else
+        for (int t = i; t <= m; t++)
+            sorted.at(k++) = arr->at(t);
+        
+    for (int t = l; t <= r; t++)
+        arr->at(t) = sorted.at(t);      
+}
+
+void MergeSort(vector<pair<int, string> > * arr, int l, int r)
+{
+    if(l < r)
+    {
+        int m = (l + r) / 2;
+
+        MergeSort(arr, l, m);
+        MergeSort(arr, m + 1, r);
+
+        Merge(arr, l, m, r);
+    }
+}
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     int n;
     cin >> n;
-    vector<pair<int, string>> arr(n);
+    vector<pair<int, string> > arr(n);
 
     for (int i = 0; i < n; i++)
-    {
-        int ageTmp;
-        string nameTmp;
+        cin >> arr.at(i).first >> arr.at(i).second;
 
-        cin >> ageTmp >> nameTmp;
-
-        arr.at(i) = pair<int, string>(ageTmp, nameTmp);
-    }
+    MergeSort(&arr, 0, n - 1);
 
     for (int i = 0; i < n; i++)
         cout << arr.at(i).first << " " << arr.at(i).second << '\n';
