88ada7bb98e68e65469c8a692fda7444c6589d4e
mw08081 <president304@naver.com>
2022-03-03 13:08:04 +0900

    added 9093

---
+++ BAEKJOON/Q9093/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    string str;
+    stack<char> res;
+
+    int to = 0;
+    for (int i = 0; i < t; i++)
+    {
+        getline(cin, str);
+        str += ' ';
+        
+        for (int i = 0; i < str.length(); i++)
+        {
+            if(str[i] == ' ')
+            {
+                while(!res.empty())
+                {
+                    cout << res.top();
+                    res.pop();
+                }
+                cout << " ";    
+            }
+            else
+                res.push(str[i]);
+            
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
